88b22c0d359103ba1031a31c69f9235ab4c716a9
Mesutio <mesutibisoglu@gmail.com>
2023-03-29 21:37:57 +0300

    first commit

---
+++ react-admin/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import { ProSidebarProvider } from 'react-pro-sidebar';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+ <BrowserRouter>
+ <ProSidebarProvider>   <App />
+ </ProSidebarProvider>
+ </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ react-admin/src/scenes/dashboard/dashboard.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+function Dashboard(){
+    return (
+       <Box m="20px">
+  <Box>
+   
+  </Box>
+       </Box>
+    );
+}
+export default Dashboard;
